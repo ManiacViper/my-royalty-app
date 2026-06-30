b2d5f6b40e00ff764e07639c28381114cd11ea10
Kenneth Monteiro <kenneth.d.monteiro@gmail.com>
2026-06-30 13:23:42 +0100

    rendering all top 100 songs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,28 @@
 import SongsTable from "./components/client/song-table";
+import { getTopSongs } from "./components/server/songs";
 
-export default function Home() {
-  const songs = [
-    {
-      id: 1,
-      title: "some-song-name",
-      author: "some-author-name",
-      progress: 0.27,
-    },
-    {
-      id: 2,
-      title: "some-song-name-2",
-      author: "some-author-name-2",
-      progress: 0.6,
-    },
-  ];
+export default async function Home() {
+  // const songs = [
+  //   {
+  //     id: 1,
+  //     title: "some-song-name",
+  //     author: "some-author-name",
+  //     progress: 0.27,
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "some-song-name-2",
+  //     author: "some-author-name-2",
+  //     progress: 0.6,
+  //   },
+  // ];
+
+  const topSongs = await getTopSongs();
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <SongsTable songs={songs}></SongsTable>
+        <SongsTable songs={topSongs}></SongsTable>
       </main>
     </div>
   );
